44d72bb43ff02e5422ea3acfaee6f8dac3902f98
DogukanGun <dogukangundogan5@gmail.com>
2025-05-12 20:56:07 +0200

    Update package dependencies to React 18 and Next.js 14

--- nexagreement/app/api/auth/[...civicauth]/route.ts
+++ nexagreement/app/api/auth/[...civicauth]/route.ts
@@ -1,3 +1,7 @@
 import { handler } from "@civic/auth-web3/nextjs"
 
+export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
+
 export const GET = handler()
+export const POST = handler()
