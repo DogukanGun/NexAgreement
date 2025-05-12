17ab044ad62e4fd3560daaedde62c6351851e87f
DogukanGun <dogukangundogan5@gmail.com>
2025-05-12 20:45:32 +0200

    fix middleware

--- nexagreement/middleware.ts
+++ nexagreement/middleware.ts
@@ -4,6 +4,6 @@ export default authMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next|api/auth/callback|favicon.ico|sitemap.xml|robots.txt|.*\\.jpg|.*\\.png|.*\\.svg|.*\\.gif).*)',
+    '/((?!_next|favicon.ico|sitemap.xml|robots.txt|.*\\.jpg|.*\\.png|.*\\.svg|.*\\.gif).*)',
   ], 
 }
